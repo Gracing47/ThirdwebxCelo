08ccb7739756f93037e3c35ac656b4feba69188d
Gracing47 <thomas.fosu@icloud.com>
2023-09-15 20:15:17 +0200

    main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,19 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { CeloAlfajoresTestnet } from "@thirdweb-dev/chains";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "CeloAlfajoresTestnet";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      activeChain={activeChain}
-    >
+    activeChain={CeloAlfajoresTestnet}
+    clientId="your-client-id"
+  >
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
